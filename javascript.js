6121b70a7112a6906cc07b6f111ccf48ed486db0
disasterdevy <36893414+disasterdevy@users.noreply.github.com.>
2024-01-20 14:20:45 +0100

    Add while-loop and counter to declare winner properly

--- javascript.js
+++ javascript.js
@@ -8,11 +8,14 @@
 // 
 // 
 
-let randomNum = Math.floor(Math.random()*100);
+let randomNum;
 let computerInput;
 let userInput;
+let computerWins = 0;
+let userWins = 0;
 
 function getComputerChoice(){
+    randomNum = Math.floor(Math.random()*100);
     console.log(randomNum);
     if (randomNum < 34){
         computerInput = "rock";
@@ -37,49 +40,62 @@ function chooseWinner(){
     if (computerInput == "rock"){
         if(userInput == "paper"){
             console.log("You win! Paper beats rock");
+            userWins = ++userWins;
         }
         else if(userInput == "scissors"){
             console.log("You lose! Rock beats scissors");
+            computerWins = ++computerWins;
         }
         else{
             console.log("It's a tie! play again");
-            getComputerChoice();
-            getUserChoice();
-            chooseWinner();
+            game();
         }
     }
     // computer chooses paper
     if (computerInput == "paper"){
         if(userInput == "scissors"){
             console.log("You win! Scissors beats paper");
+            userWins = ++userWins;
         }
         else if(userInput == "rock"){
             console.log("You lose! Paper beats rock");
+            computerWins = ++computerWins;
         }
         else{
             console.log("It's a tie! play again");
-            getComputerChoice();
-            getUserChoice();
-            chooseWinner();
+            game();
         }
     }
     // computer chooses scissors
     if (computerInput == "scissors"){
         if(userInput == "rock"){
             console.log("You win! Rock beats scissors");
+            userWins = ++userWins;
         }
         else if(userInput == "paper"){
             console.log("You lose! Scissors beats paper");
+            computerWins = ++computerWins;
         }
         else{
             console.log("It's a tie! play again");
-            getComputerChoice();
-            getUserChoice();
-            chooseWinner();
+            game();
         }
     }
 }
 
-getComputerChoice();
-getUserChoice();
-chooseWinner();
+function game(){
+    while(computerWins < 3 && userWins < 3){
+        getComputerChoice();
+        getUserChoice();
+        chooseWinner();
+        console.log(computerWins, userWins);
+    }
+    if (computerWins == 3){
+        console.log("The machine has bested you!");
+    }
+    else{
+        console.log("You bested the machine! Congratulations.");
+    }
+}
+
+game();
